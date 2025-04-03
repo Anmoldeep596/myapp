37e1b7c865f2255d3f4f84e875cc8468341a36b1
anmolsg4243 <anmolsg4243@gmail.com>
2025-04-03 20:49:32 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/firebase_options.dart';
 import 'package:myapp/screens/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  //Initialize Flutter bindings
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //initialize Firebase with platform default options
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
